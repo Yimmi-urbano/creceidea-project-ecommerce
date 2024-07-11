45de0c663db9d0726fc52888151648f7e097372b
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-07-10 22:25:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
-import dotenv from 'dotenv';
-dotenv.config();
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
-  }).listen(30001, (err) => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:30001');
-  });
+    createServer((req, res) => {
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query } = parsedUrl;
+        if (pathname === '/api/products') {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ message: 'API endpoint for products' }));
+        } else {
+            handle(req, res, parsedUrl);
+        }
+    }).listen(30001, (err) => {
+        if (err) throw err;
+        console.log('> Ready on http://localhost:30001');
+    });
 });
