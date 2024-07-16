54e3cf4e8023abbcdc3f0f53206f3c677eebb9e4
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-07-16 01:25:15 -0500

    fix/login

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     cacheHandler: require.resolve('./cache-handler.js'),
-    cacheMaxMemorySize: 0, // disable default in-memory caching
+    cacheMaxMemorySize: 100, // disable default in-memory caching
 }
 
 module.exports = nextConfig
