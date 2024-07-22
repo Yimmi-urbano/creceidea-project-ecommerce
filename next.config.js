3acb3a9b37d7fe6816fd929b04b97f8133e8b9a6
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-07-22 16:32:53 -0500

    fix/export

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
     cacheHandler: require.resolve('./cache-handler.js'),
     cacheMaxMemorySize: 100, // disable default in-memory caching
+    output:"export"
 }
 
 module.exports = nextConfig
