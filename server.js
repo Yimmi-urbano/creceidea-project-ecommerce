ca5915e5291cee1a9e4dbf8a689e1aa034ed4f65
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-07-22 15:19:29 -0500

    fix/port

--- server.js
+++ server.js
@@ -3,21 +3,16 @@ const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;  // Usa el puerto definido en las variables de entorno o el puerto 3000 por defecto
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
     createServer((req, res) => {
         const parsedUrl = parse(req.url, true);
-        const { pathname, query } = parsedUrl;
-        if (pathname === '/api/products') {
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({ message: 'API endpoint for products' }));
-        } else {
-            handle(req, res, parsedUrl);
-        }
-    }).listen(30001, (err) => {
+        handle(req, res, parsedUrl);
+    }).listen(port, (err) => {
         if (err) throw err;
-        console.log('> Ready on http://localhost:30001');
+        console.log(`> Ready on http://localhost:${port}`);
     });
 });
